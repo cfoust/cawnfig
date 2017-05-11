c4db277356f04ad61909db5eb2e32696effca6bb
Caleb Foust <cfoust@sqweebloid.com>
2017-05-11 09:12:55 -0500

    Include plugin manager scripts
    Also move .vimrc into the .vim folder and shuffle some
    things around in i3's config.

---
+++ tmux/install-tpm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Install the TMUX plugin manager.
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
---
+++ vim/install-vundle.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Clone down Vundle, the package manager for vim.
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
