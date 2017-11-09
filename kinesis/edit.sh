fb8fcd622cef91518f1f4691f6e73fbd570f577e
Caleb Foust <cfoust@sqweebloid.com>
2017-11-09 11:18:52 -0600

    Add Kinesis Advantage configuration files

---
+++ kinesis/edit.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+# This script makes it easy to edit your Kinesis
+# configuration on Linux.
+
+# Mount the keyboard
+device=$(lsblk | grep 2M | cut -d' ' -f1)
+if [ -z "$device" ]; then
+  echo "Keyboard not connected."
+  exit 0
+fi
+
+sudo mkdir -p /media/kinesis
+sudo mount /dev/$device /media/kinesis
+sudo vim /media/kinesis/active
+sudo cp /media/kinesis/active/*_qwerty.txt .
+sudo umount /media/kinesis
