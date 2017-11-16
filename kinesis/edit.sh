b72d0636588ed819d2f08c0a8112dfd309b4b425
Caleb Foust <cfoust@sqweebloid.com>
2017-11-16 11:09:15 -0600

    Add more developer keybinds and fix WoW's setup

--- kinesis/edit.sh
+++ kinesis/edit.sh
@@ -11,6 +11,6 @@ fi
 
 sudo mkdir -p /media/kinesis
 sudo mount /dev/$device /media/kinesis
-sudo vim /media/kinesis/active
+sudo vim /media/kinesis/active/d_qwerty.txt
 sudo cp /media/kinesis/active/*_qwerty.txt .
 sudo umount /media/kinesis
