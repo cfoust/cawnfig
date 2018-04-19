280d1d141ef4e3d1bf1c1617dd7bb0472a1a584c
Caleb Foust <cfoust@sqweebloid.com>
2018-04-19 04:38:17 -0700

    maybe working vim splash

--- configs/polychromat/pcm.sh
+++ configs/polychromat/pcm.sh
@@ -6,9 +6,16 @@
 # everything into one.
 # PCM -- far more curative than TCM.
 
+# These should not be overridden.
 export PCM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PCM_VIM_STARTUP="toilet -f future STRENGTH"
+export PCM_VIM_STARTUP_SCRIPT="$PCM_DIR/vim_splash"
+
+# This is the command that's run to generate the vim splash screen.
+# polychromat assumes that the output of this command is 80 characters
+# wide so that it can center it on the screen.
+export PCM_VIM_STARTUP="toilet -f smmono9 Призрак бродит по Европе - призрак коммунизма."
 export PCM_VIM_THEME="base16-cupcake"
+export PCM_VIM_THEME="wal"
 
 tmux set-option -g @ot-ui:margin-command \
   "bash $PCM_DIR/themes/means_of_production"
