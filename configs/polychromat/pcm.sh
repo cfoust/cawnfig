da839d7e18b5411caf3dfc7018d1a4898eb951aa
Caleb Foust <cfoust@sqweebloid.com>
2018-04-19 05:47:13 -0700

    Centering for the vim startup screen in polychromat

--- configs/polychromat/pcm.sh
+++ configs/polychromat/pcm.sh
@@ -13,7 +13,7 @@ export PCM_VIM_STARTUP_SCRIPT="$PCM_DIR/vim_splash"
 # This is the command that's run to generate the vim splash screen.
 # polychromat assumes that the output of this command is 80 characters
 # wide so that it can center it on the screen.
-export PCM_VIM_STARTUP="toilet -f smmono9 Призрак бродит по Европе - призрак коммунизма."
+export PCM_VIM_SPLASH="toilet -f smmono9 Призрак бродит по Европе - призрак коммунизма."
 export PCM_VIM_THEME="base16-cupcake"
 export PCM_VIM_THEME="wal"
 
