5c02f36c2761b7141a627aad1d266862861f02aa
Caleb Foust <cfoust@sqweebloid.com>
2021-09-28 13:42:00 -0700

    A bunch of changes for sncli

--- configs/polychromat/pcm.sh
+++ configs/polychromat/pcm.sh
@@ -23,9 +23,6 @@ unset PCM_VIM_SPLASH
 # The vim color scheme to use.
 unset PCM_VIM_THEME
 
-# The konsole color scheme to use.
-unset PCM_KONSOLE_THEME
-
 #############################################
 
 export PCM_THEME="yahtzee"
@@ -41,34 +38,7 @@ if [ -f "$APPLY" ]; then
   . $APPLY
 fi
 
-# Computes the hex version of the background color of the
-# iTerm scheme and sets the tmux pane borders to it.
-# This is so we don't see any pane borders.
-set_tmux_pane_border() {
-  local theme=${1:-$PCM_KONSOLE_THEME} line=7
-
-  # Special case for `wal` themes
-  if [ "$theme" == "colors-konsole" ]; then
-    line=2
-  fi
-
-  theme_file="$PCM_DIR/../konsole/share/$theme.colorscheme"
-  color_line=$(sed "${line}q;d" "$theme_file")
-  [[ "$color_line" =~ ([0-9]+),([0-9]+),([0-9]+) ]]
-  r="${BASH_REMATCH[1]}"
-  g="${BASH_REMATCH[2]}"
-  b="${BASH_REMATCH[3]}"
-  hex=\#$(printf "%02X%02X%02X" $r $g $b)
-  tmux set -g pane-active-border-style fg="$hex",bg="$hex"
-  tmux set -g pane-border-style fg="$hex",bg="$hex"
-}
-
 if [ ! -z "$TMUX" ]; then
-  # Set the pane borders to the background color of the konsole theme.
-  if [ ! -z "$PCM_KONSOLE_THEME" ]; then
-    set_tmux_pane_border
-  fi
-
   if [ -f "$PCM_THEME_DIR/bg" ]; then
     tmux set-option -g @ot-ui:margin-command "$PCM_THEME_DIR/bg"
   fi
